6f957aa5a0ebb62c7a3079dd5ad807d1036e0806
ifonseca <lamgers@gmail.com>
2016-03-29 18:23:12 -0600

    prototipo dos terminado

--- AmigosAire/apps/main/urls.py
+++ AmigosAire/apps/main/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from .views import(
 	IndexView,
+	PlanView,
 	)
 
 urlpatterns = [   
 	url(r'^$', IndexView.as_view(), name='index'),
+	url(r'^plan/$', PlanView.as_view(), name='plan'),
 ]
--- AmigosAire/apps/main/views.py
+++ AmigosAire/apps/main/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class PlanView(TemplateView):
+    template_name = "plan.html"
