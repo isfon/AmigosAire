482d3a7aa5cda5c432dbbd3617557dc74843d5de
Israel Antonio Fonseca Gomez <lamgers@gmail.com>
2016-04-20 01:28:05 -0500

    Update urls.py

--- AmigosAire/apps/main/urls.py
+++ AmigosAire/apps/main/urls.py
@@ -14,9 +14,5 @@ urlpatterns = [
 	url(r'^$', IndexView.as_view(), name='index'),
 	url(r'^faq/$', PreguntasView.as_view(), name='faq'),
 	url(r'^privacidad/$', PrivacidadView.as_view(), name='privacidad'),
-	url(r'^planes/$', PlanesView.as_view(), name='planes'),
-	url(r'^galeria/$', GaleriaView.as_view(), name='galeria'),
-	url(r'^contacto/$', ContactoView.as_view(), name='contacto'),
-	url(r'^enviar/$', EnviarMensaje.as_view()),
 	url(r'^plan/(?P<slug>.*)/$', PlanDetailView.as_view(), name='plan_detalle'),
 ]
